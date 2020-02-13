7e2a2936e396407313941d267fa3c2f1bc3f99ff
unknown <iikuboazusa@gmail.com>
2020-02-13 23:02:50 +0900

    Selection sort

---
+++ Sorting/Selection.rb
@@ -0,0 +1,23 @@
+require 'benchmark'
+input_array = Array.new(100){rand(1...100)} 
+print (input_array)
+
+def sellection_sort(input_array)
+    for i in 0...input_array.length-1
+        j=i
+        while j>0
+            if input_array[j-1]>input_array[j]
+                temp = input_array[j]
+                input_array[j] = input_array[i]
+                input_array[i] = temp
+            end
+            j=j-1
+        end
+    end
+    print (input_array)
+end
+
+time = Benchmark.measure {
+    sellection_sort(input_array)
+   }
+   puts time.real
